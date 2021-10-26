f425a1071cf31312f8a68ad929286a1631072bf5
Lyman Johnson <lyman.johnson@gmail.com>
2021-10-26 13:47:09 -0400

    Main view is now dummy login page with username, password, and submit button elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:compatica/forms.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,15 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Form Validation Demo';
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
